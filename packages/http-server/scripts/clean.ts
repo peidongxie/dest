117ed6199f4118d1058e28515ebff5184b013227
谢沛东 <peidongxie@outlook.com>
2022-07-07 01:04:56 +0800

    chore: 实现清理产物脚本

---
+++ packages/http-server/scripts/clean.ts
@@ -0,0 +1,28 @@
+import { readdirSync, removeSync, statSync } from 'fs-extra';
+import { join } from 'path';
+
+const getBuildFiles = (dir: string): string[] => {
+  const files = readdirSync(dir);
+  return files
+    .map((file) => {
+      const path = join(dir, file);
+      const stats = statSync(path);
+      if (stats.isDirectory()) {
+        if (path === 'node_modules') return null;
+        if (path === 'chunks') return path;
+        return getBuildFiles(path);
+      } else if (stats.isFile()) {
+        if (/\.d\.ts$/.test(file)) return path;
+        if (/\.js$/.test(file)) return path;
+        if (/tsconfig\.tsbuildinfo/.test(file)) return path;
+        return null;
+      }
+      return null;
+    })
+    .filter((v): v is string[] | string => v !== null)
+    .flat();
+};
+
+for (const file of getBuildFiles('.')) {
+  removeSync(file);
+}
