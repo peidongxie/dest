fea7dce1fff873371f867e6be04e38bfd79e69ba
谢沛东 <peidongxie@outlook.com>
2022-08-27 23:28:34 +0800

    feat: 实现 grpc 启动关闭

---
+++ packages/grpc-server/src/server/index.ts
@@ -0,0 +1,40 @@
+import { Server as GrpcServer, ServerCredentials } from '@grpc/grpc-js';
+
+class Server {
+  private originalValue: GrpcServer;
+
+  public constructor() {
+    this.originalValue = new GrpcServer();
+  }
+
+  public async close(): Promise<GrpcServer> {
+    return new Promise((resolve, reject) =>
+      this.originalValue.tryShutdown((e) => {
+        if (e) {
+          reject(e);
+        } else {
+          resolve(this.originalValue);
+        }
+      }),
+    );
+  }
+
+  public listen(port?: number, hostname?: string): Promise<GrpcServer> {
+    return new Promise((resolve, reject) =>
+      this.originalValue.bindAsync(
+        `${hostname || 'localhost'}:${port || 0}`,
+        ServerCredentials.createInsecure(),
+        (e) => {
+          if (e) {
+            reject(e);
+          } else {
+            this.originalValue.start();
+            resolve(this.originalValue);
+          }
+        },
+      ),
+    );
+  }
+}
+
+export { Server as default };
