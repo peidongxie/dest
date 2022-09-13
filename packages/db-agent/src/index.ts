530530b4ee4c395a90261766fd3c86893b7ef748
谢沛东 <peidongxie@outlook.com>
2022-09-13 23:39:09 +0800

    feat: 优化 http api 类型

--- packages/db-agent/src/index.ts
+++ packages/db-agent/src/index.ts
@@ -14,14 +14,14 @@ const createInitialDatabase = async () => {
 };
 
 const startServer = async () => {
-  const cors = new Cors();
-  const router = new Router();
+  const cors = new Cors<'HTTP'>();
+  const router = new Router<'HTTP'>();
   router.setRoute('DELETE', '/database', deleteDatabase);
   router.setRoute('GET', '/database', getDatabase);
   router.setRoute('POST', '/database', postDatabase);
   router.setRoute('PUT', '/database', putDatabase);
   router.setRoute('POST', '/query', postQuery);
-  const server = new Server('http');
+  const server = new Server<'HTTP'>('http');
   server.use(cors.getHandler());
   server.use(router.getHandler());
   server.listen(3001);
