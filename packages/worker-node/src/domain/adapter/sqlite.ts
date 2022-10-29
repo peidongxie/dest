ae0d1ff420c1a7eeeb709005173b797d53460547
谢沛东 <peidongxie@outlook.com>
2022-10-30 00:36:38 +0800

    feat: 数据库全量数据快照

--- packages/worker-node/src/domain/adapter/sqlite.ts
+++ packages/worker-node/src/domain/adapter/sqlite.ts
@@ -65,26 +65,51 @@ class Sqlite implements Adapter {
     return null;
   }
 
+  async getSnapshot() {
+    if (this.name) {
+      const result: { name: string }[] = await (
+        this.readable as DataSource
+      ).query(
+        `SELECT name FROM sqlite_master WHERE type = "table" AND name != "sqlite_sequence"`,
+      );
+      const entries = await Promise.all(
+        result.map(
+          async (row): Promise<[string, unknown[]]> => [
+            row.name,
+            await (this.readable as DataSource).query(
+              `SELECT * FROM ${row.name}`,
+            ),
+          ],
+        ),
+      );
+      return Object.fromEntries(entries);
+    } else {
+      const files = await readdir(dir);
+      const entries = files
+        .filter((file) => file.endsWith('.sqlite'))
+        .map((file) => [file.replace(/.sqlite$/, ''), []]);
+      return Object.fromEntries(entries);
+    }
+  }
+
   getWritableDataSource() {
     return this.writable;
   }
 
-  async postDestroy() {
-    if (this.name) {
-      const file = this.name + '.sqlite';
-      await rm(join(dir, file), { force: true });
-    }
+  async postCreate() {
+    if (this.name) return;
+    const files = await readdir(dir);
+    await Promise.all(
+      files.map((file) => {
+        return rm(join(dir, file), { recursive: true, force: true });
+      }),
+    );
   }
 
-  async postCreate() {
-    if (!this.name) {
-      const files = await readdir(dir);
-      await Promise.all(
-        files.map((file) => {
-          return rm(join(dir, file), { recursive: true, force: true });
-        }),
-      );
-    }
+  async postDestroy() {
+    if (!this.name) return;
+    const file = this.name + '.sqlite';
+    await rm(join(dir, file), { force: true });
   }
 }
 
