b6ed3f35012553c54c203b3fb8119556e245214f
谢沛东 <peidongxie@outlook.com>
2022-11-11 21:33:43 +0800

    chore: 实现 proto 构建脚本

--- packages/worker-node/scripts/build.ts
+++ packages/worker-node/scripts/build.ts
@@ -1,4 +1,31 @@
+import { execSync } from 'child_process';
 import { build, type BuildOptions } from 'esbuild';
+import { existsSync, readdirSync, statSync } from 'fs-extra';
+import { join } from 'path';
+
+const getEntryPoints = (dir: string): string[] => {
+  const files = readdirSync(dir);
+  return files
+    .map((file) => {
+      const path = join(dir, file);
+      const stats = statSync(path);
+      if (!stats.isDirectory()) return null;
+      if (!existsSync(join(path, 'proto.proto'))) return null;
+      return path;
+    })
+    .filter((v): v is string => v !== null);
+};
+
+const getCommand = (entryPoint: string): string => {
+  return [
+    'grpc_tools_node_protoc',
+    '--proto_path=' + entryPoint,
+    '--plugin=./node_modules/.bin/protoc-gen-ts_proto',
+    '--ts_proto_out=' + entryPoint,
+    '--ts_proto_opt=esModuleInterop=true,outputServices-grpc-js',
+    join(entryPoint, 'proto.proto'),
+  ].join(' ');
+};
 
 const buildOptions: BuildOptions = {
   bundle: true,
@@ -22,5 +49,10 @@ const buildOptions: BuildOptions = {
 };
 
 (async () => {
+  const entryPoints = getEntryPoints('./src/controller');
+  for (const entryPoint of entryPoints) {
+    const command = getCommand(entryPoint);
+    execSync(command);
+  }
   await build(buildOptions);
 })();
