0a330518f7a72ed5a33c1f1845f6efec72c2a2d2
谢沛东 <peidongxie@outlook.com>
2022-11-12 23:44:10 +0800

    feat: 优化 proto 构建

--- packages/worker-node/scripts/build.ts
+++ packages/worker-node/scripts/build.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 import { build, type BuildOptions } from 'esbuild';
-import { existsSync, readdirSync, statSync } from 'fs-extra';
-import { join } from 'path';
+import { readdirSync, statSync } from 'fs-extra';
+import { dirname, extname, join, relative } from 'path';
 
 const getEntryPoints = (dir: string): string[] => {
   const files = readdirSync(dir);
@@ -9,22 +9,52 @@ const getEntryPoints = (dir: string): string[] => {
     .map((file) => {
       const path = join(dir, file);
       const stats = statSync(path);
-      if (!stats.isDirectory()) return null;
-      if (!existsSync(join(path, 'proto.proto'))) return null;
-      return path;
+      if (stats.isDirectory()) {
+        return getEntryPoints(path).map((entryPoint) => join(path, entryPoint));
+      }
+      if (stats.isFile()) {
+        if (extname(file) !== '.proto') return null;
+        return path.substring(0, path.length - 6);
+      }
+      return null;
     })
-    .filter((v): v is string => v !== null);
+    .filter((v): v is string[] | string => v !== null)
+    .flat()
+    .map((entryPoint) => relative(dir, entryPoint));
 };
 
-const getCommand = (entryPoint: string): string => {
-  return [
+const getCommands = (entryPoint: string): string[] => {
+  const protoPath = join('protos', entryPoint + '.proto');
+  const protoDir = dirname(protoPath);
+  const sourcePath = join('protos', entryPoint + '.ts');
+  const targetPath = join('src/controller', entryPoint, 'proto.ts');
+  const protoc = [
     'grpc_tools_node_protoc',
-    '--proto_path=' + entryPoint,
-    '--plugin=./node_modules/.bin/protoc-gen-ts_proto',
-    '--ts_proto_out=' + entryPoint,
+    '--proto_path=' + protoDir,
+    '--plugin=node_modules/.bin/protoc-gen-ts_proto',
+    '--ts_proto_out=' + protoDir,
     '--ts_proto_opt=esModuleInterop=true,outputServices-grpc-js',
-    join(entryPoint, 'proto.proto'),
+    protoPath,
   ].join(' ');
+  const mv = ['mv', sourcePath, targetPath].join(' ');
+  const sed = [
+    'sed',
+    '-i',
+    '""',
+    '-e',
+    '1d',
+    '-e',
+    '"s/extends {}/extends Record<string, unknown>/g"',
+    '-e',
+    '"s/| Function/| ((...args: never[]) => void)/g"',
+    '-e',
+    '"s/value: any/value: unknown/g"',
+    '-e',
+    '"s/: any/: Record<string, unknown>/g"',
+    targetPath,
+  ].join(' ');
+  const eslint = ['eslint', '--fix', targetPath].join(' ');
+  return [protoc, mv, sed, eslint];
 };
 
 const buildOptions: BuildOptions = {
@@ -49,10 +79,12 @@ const buildOptions: BuildOptions = {
 };
 
 (async () => {
-  const entryPoints = getEntryPoints('./src/controller');
+  const entryPoints = getEntryPoints('protos');
   for (const entryPoint of entryPoints) {
-    const command = getCommand(entryPoint);
-    execSync(command);
+    const commands = getCommands(entryPoint);
+    for (const command of commands) {
+      execSync(command);
+    }
   }
   await build(buildOptions);
 })();
