ab5b7440a5b7260e27202f5d01bb1ffaed8751c1
谢沛东 <peidongxie@outlook.com>
2023-01-12 19:16:24 +0800

    feat: 优化不必要的查询双引号和异步函数

--- packages/worker-node/src/index.ts
+++ packages/worker-node/src/index.ts
@@ -19,31 +19,16 @@ import {
 import { AdapterType, DatabaseAction } from './domain';
 import { createDatabase, createMemo, createServer } from './service';
 
+createMemo<AdapterType>(['type', BaseType.MARIADB.toString()], 'mariadb');
+createMemo<AdapterType>(['type', BaseType.MYSQL8.toString()], 'mysql:8');
+createMemo<AdapterType>(['type', BaseType.SQLITE.toString()], 'sqlite');
+createMemo<DatabaseAction>(['action', EventAction.SAVE.toString()], 'save');
+createMemo<DatabaseAction>(['action', EventAction.REMOVE.toString()], 'remove');
+createMemo<DatabaseAction>(['action', EventAction.READ.toString()], 'read');
+createMemo<DatabaseAction>(['action', EventAction.WRITE.toString()], 'write');
+createMemo<DatabaseAction>(['action', EventAction.ROOT.toString()], 'root');
 // await createDatabase('mariadb', '', []);
 await createDatabase('sqlite', '', []);
-await createMemo<AdapterType>(['type', BaseType.MARIADB.toString()], 'mariadb');
-await createMemo<AdapterType>(['type', BaseType.MYSQL8.toString()], 'mysql:8');
-await createMemo<AdapterType>(['type', BaseType.SQLITE.toString()], 'sqlite');
-await createMemo<DatabaseAction>(
-  ['action', EventAction.SAVE.toString()],
-  'save',
-);
-await createMemo<DatabaseAction>(
-  ['action', EventAction.REMOVE.toString()],
-  'remove',
-);
-await createMemo<DatabaseAction>(
-  ['action', EventAction.READ.toString()],
-  'read',
-);
-await createMemo<DatabaseAction>(
-  ['action', EventAction.WRITE.toString()],
-  'write',
-);
-await createMemo<DatabaseAction>(
-  ['action', EventAction.ROOT.toString()],
-  'root',
-);
 await createServer(
   [
     deleteAgentByHttp,
