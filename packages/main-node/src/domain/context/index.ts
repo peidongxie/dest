7d48196d1e0d213eed1f24d95933a4203cb17f70
谢沛东 <peidongxie@outlook.com>
2023-01-17 20:51:44 +0800

    feat: 补全主节点 context 相关类型

--- packages/main-node/src/domain/context/index.ts
+++ packages/main-node/src/domain/context/index.ts
@@ -1,15 +1,2 @@
-import { type ContextEvent, type ContextType } from './type';
-
-class Context {
-  private name: string;
-  private events: ContextEvent[];
-  private type: ContextType;
-
-  constructor(type: ContextType, name?: string, events?: ContextEvent[]) {
-    this.type = type;
-    this.name = name || '';
-    this.events = events || [];
-  }
-}
-
-export { Context, type ContextEvent, type ContextType };
+export * from './context';
+export * from './type';
