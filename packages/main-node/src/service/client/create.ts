083efd46d6d09733f75eb71b8e5b2873aff3f70f
谢沛东 <peidongxie@outlook.com>
2023-02-03 19:33:22 +0800

    feat: 取消 client 调度

--- packages/main-node/src/service/client/create.ts
+++ packages/main-node/src/service/client/create.ts
@@ -1,27 +1,30 @@
-import { randomUUID } from 'crypto';
-import { HttpClient, RpcClient, Scheduler, type Client } from '../../domain';
+import { HttpClient, RpcClient, type Client } from '../../domain';
+import { readContexts } from '../context';
 import { createMemo } from '../memo';
 
 const createClient = (
-  api: 'http' | 'rpc',
-  port: number,
-  hostname: string,
-  secret = '',
-): Promise<Scheduler<Client> | null> | null => {
-  const scheduler =
-    api === 'http'
-      ? new Scheduler(new HttpClient(port, hostname))
-      : api === 'rpc'
-      ? new Scheduler(new RpcClient(port, hostname))
+  token: string,
+  setup: {
+    api: 'http' | 'rpc';
+    port: number;
+    hostname: string;
+    secret?: string;
+  },
+): Promise<Client> | null => {
+  const client =
+    setup.api === 'http'
+      ? createMemo(['client', token], new HttpClient(token, setup))
+      : setup.api === 'rpc'
+      ? createMemo(['client', token], new RpcClient(token, setup))
       : null;
-  if (!scheduler) return null;
-  const promise = scheduler.runTask(async (client) => {
-    const token = randomUUID();
-    const { success } = await client.postAgent(secret, token);
-    if (!success) return null;
-    return createMemo(['client', token], scheduler);
-  });
-  return promise;
+  if (!client) return null;
+  const promises = [];
+  const schedulers = readContexts();
+  for (const scheduler of schedulers) {
+    const promise = scheduler.runTask((context) => context.addClient(client));
+    promises.push(promise);
+  }
+  return Promise.allSettled(promises).then(() => client);
 };
 
 export { createClient };
--- packages/main-node/src/service/client/read.ts
+++ packages/main-node/src/service/client/read.ts
@@ -1,12 +1,12 @@
-import { randomInt } from 'crypto';
-import { type Client, type Scheduler } from '../../domain';
+import { type Client } from '../../domain';
 import { readMemo, readMemos } from '../memo';
 
-const readClient = (token?: string): Scheduler<Client> | null => {
-  if (token) return readMemo<Scheduler<Client>>(['client', token]);
-  const schedulers = readMemos<Scheduler<Client>>(['client']);
-  if (schedulers.length === 0) return null;
-  return schedulers[randomInt(schedulers.length)];
+const readClient = (token: string): Client | null => {
+  return readMemo<Client>(['client', token]);
 };
 
-export { readClient };
+const readClients = (): Client[] => {
+  return readMemos<Client>(['client']);
+};
+
+export { readClient, readClients };
