1296b8b8a6ddfc701b3fa320caa1289d095e99b7
谢沛东 <peidongxie@outlook.com>
2023-02-05 09:43:10 +0800

    feat: 利用 condition 构造 actuality

--- packages/main-node/src/service/actuality/create.ts
+++ packages/main-node/src/service/actuality/create.ts
@@ -1,23 +1,43 @@
-import { randomUUID } from 'crypto';
 import {
   type Actuality,
-  type ClientResult,
-  type ClientSnapshot,
+  type ActualityCondition,
+  type ClientType,
 } from '../../domain';
+import { readContext } from '../context';
 import { createMemo } from '../memo';
 
-const createActuality = <T>(
-  readWriteResult: ClientResult<T>,
-  introspectResult?: ClientResult<ClientSnapshot>,
-): Actuality<T> | null => {
-  const uuid = randomUUID();
-  return createMemo(['actuality', uuid], {
-    uuid,
-    error: readWriteResult.error || introspectResult?.error || '',
-    rows: readWriteResult.rows,
-    snapshots: introspectResult?.rows || [],
-    time: readWriteResult.time,
-  });
+const createReadActuality = <T>(
+  type: ClientType,
+  name: string,
+  condition: ActualityCondition,
+): Promise<Actuality<T>> | null => {
+  const scheduler = readContext(type, name);
+  if (!scheduler) return null;
+  const promise = scheduler.runTask(async (context) => {
+    const actuality = await context.read<T>(condition.query, condition.values);
+    createMemo(['actuality', actuality.uuid], actuality);
+    return actuality;
+  }, true);
+  return promise;
 };
 
-export { createActuality };
+const createWriteActuality = <T>(
+  type: ClientType,
+  name: string,
+  condition: ActualityCondition,
+): Promise<Actuality<T>> | null => {
+  const scheduler = readContext(type, name);
+  if (!scheduler) return null;
+  const promise = scheduler.runTask(async (context) => {
+    const actuality = await context.write<T>(
+      condition.query,
+      condition.values,
+      condition.tables,
+    );
+    createMemo(['actuality', actuality.uuid], actuality);
+    return actuality;
+  }, true);
+  return promise;
+};
+
+export { createReadActuality, createWriteActuality };
