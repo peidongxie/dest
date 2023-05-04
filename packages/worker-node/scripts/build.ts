f2ed34beb78c6f8d7aedf433858e50d46d209e78
谢沛东 <peidongxie@outlook.com>
2023-05-04 14:36:18 +0800

    chore: 在 worker 构建镜像前删除已有镜像

---
+++ packages/worker-node/scripts/build.ts
@@ -0,0 +1,45 @@
+import { spawn } from 'child_process';
+
+const buildArgs = {
+  APP_PORT: process.env.APP_PORT || '',
+  NPM_REGISTRY: process.env.NPM_REGISTRY || '',
+};
+const file = 'docker/Dockerfile';
+const tag = 'peidongxie/dest-worker-node';
+const path = '../../';
+
+const runCommand = async (
+  command: string,
+  args: string[],
+  ignore = false,
+): Promise<void> => {
+  const child = spawn(command, args);
+  if (!ignore) {
+    child.stdout.on('data', (chunk) => {
+      console.log(chunk.toString());
+    });
+    child.stderr.on('data', (chunk) => {
+      console.error(chunk.toString());
+    });
+  }
+  return new Promise((resolve) => {
+    child.on('close', resolve);
+  });
+};
+
+(async () => {
+  await runCommand('docker', ['rmi', tag], true);
+  await runCommand('docker', [
+    'build',
+    ...Object.entries(buildArgs)
+      .filter(([key, value]) => key && value)
+      .map(([key, value]) => ['--build-arg', `${key}=${value}`])
+      .flat(),
+    '-f',
+    file,
+    '-t',
+    tag,
+    '--progress=plain',
+    path,
+  ]);
+})();
